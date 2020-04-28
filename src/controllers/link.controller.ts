be9592bb7cf07460088f7aeeb547f6102a081624
gohandrogo <alondono11@gmail.com>
2020-04-27 23:03:33 -0400

    created web audio streaming api post route

--- src/controllers/link.controller.ts
+++ src/controllers/link.controller.ts
@@ -1,8 +1,10 @@
-import {Count, CountSchema, Filter, FilterExcludingWhere, repository, Where} from '@loopback/repository';
-import {del, get, getModelSchemaRef, param, patch, post, put, requestBody} from '@loopback/rest';
+import {repository} from '@loopback/repository';
+import {getModelSchemaRef, post, requestBody} from '@loopback/rest';
+import fs from 'fs';
+import {PassThrough} from 'stream';
 import {Link} from '../models';
 import {LinkRepository} from '../repositories';
-import {requestVideo as youtubeService} from '../services/AVConverter';
+import {convertToAudio, requestVideo as youtubeService} from '../services/AVConverter';
 export class LinkController {
   constructor(
     @repository(LinkRepository)
@@ -13,11 +15,12 @@ export class LinkController {
     responses: {
       '200': {
         description: 'Link model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Link)}},
+        content: {'application/json': {schema: fs.ReadStream}},
       },
     },
   })
   async create(
+    //type
     @requestBody({
       content: {
         'application/json': {
@@ -29,131 +32,139 @@ export class LinkController {
       },
     })
     link: Omit<Link, 'id'>,
-  ): Promise<Link> {
+  ) {
     console.log(link);
-    const yt = youtubeService(link.url);
-    return this.linkRepository.create(link);
+    const ref = await youtubeService(link.url);
+    const output = await convertToAudio(link.url);
+    // await this.linkRepository.create(link);
+    console.log(output, ref);
+    const d = new PassThrough();
+    fs.createReadStream(`files/${link.url}.mp3`);
+
+    d.pipe(process.stdout);
+    d.on('data', console.log)
+
   }
 
-  @get('/links/count', {
-    responses: {
-      '200': {
-        description: 'Link model count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async count(
-    @param.where(Link) where?: Where<Link>,
-  ): Promise<Count> {
-    return this.linkRepository.count(where);
-  }
+  // @get('/links/count', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Link model count',
+  //       content: {'application/json': {schema: CountSchema}},
+  //     },
+  //   },
+  // })
+  // async count(
+  //   @param.where(Link) where?: Where<Link>,
+  // ): Promise<Count> {
+  //   return this.linkRepository.count(where);
+  // }
 
-  @get('/links', {
-    responses: {
-      '200': {
-        description: 'Array of Link model instances',
-        content: {
-          'application/json': {
-            schema: {
-              type: 'array',
-              items: getModelSchemaRef(Link, {includeRelations: true}),
-            },
-          },
-        },
-      },
-    },
-  })
-  async find(
-    @param.filter(Link) filter?: Filter<Link>,
-  ): Promise<Link[]> {
-    return this.linkRepository.find(filter);
-  }
+  // @get('/links', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Array of Link model instances',
+  //       content: {
+  //         'application/json': {
+  //           schema: {
+  //             type: 'array',
+  //             items: getModelSchemaRef(Link, {includeRelations: true}),
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // })
+  // async find(
+  //   @param.filter(Link) filter?: Filter<Link>,
+  // ): Promise<Link[]> {
+  //   return this.linkRepository.find(filter);
+  // }
 
-  @patch('/links', {
-    responses: {
-      '200': {
-        description: 'Link PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Link, {partial: true}),
-        },
-      },
-    })
-    link: Link,
-    @param.where(Link) where?: Where<Link>,
-  ): Promise<Count> {
-    return this.linkRepository.updateAll(link, where);
-  }
+  // @patch('/links', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Link PATCH success count',
+  //       content: {'application/json': {schema: CountSchema}},
+  //     },
+  //   },
+  // })
+  // async updateAll(
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(Link, {partial: true}),
+  //       },
+  //     },
+  //   })
+  //   link: Link,
+  //   @param.where(Link) where?: Where<Link>,
+  // ): Promise<Count> {
+  //   return this.linkRepository.updateAll(link, where);
+  // }
 
-  @get('/links/{id}', {
-    responses: {
-      '200': {
-        description: 'Link model instance',
-        content: {
-          'application/json': {
-            schema: getModelSchemaRef(Link, {includeRelations: true}),
-          },
-        },
-      },
-    },
-  })
-  async findById(
-    @param.path.number('id') id: number,
-    @param.filter(Link, {exclude: 'where'}) filter?: FilterExcludingWhere<Link>
-  ): Promise<Link> {
-    return this.linkRepository.findById(id, filter);
-  }
+  // @get('/links/{id}', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Link model instance',
+  //       content: {
+  //         'application/json': {
+  //           schema: getModelSchemaRef(Link, {includeRelations: true}),
+  //         },
+  //       },
+  //     },
+  //   },
+  // })
+  // async findById(
+  //   @param.path.number('id') id: number,
+  //   @param.filter(Link, {exclude: 'where'}) filter?: FilterExcludingWhere<Link>
+  // ): Promise<Link> {
+  //   return this.linkRepository.findById(id, filter);
+  // }
 
-  @patch('/links/{id}', {
-    responses: {
-      '204': {
-        description: 'Link PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Link, {partial: true}),
-        },
-      },
-    })
-    link: Link,
-  ): Promise<void> {
-    await this.linkRepository.updateById(id, link);
-  }
+  // @patch('/links/{id}', {
+  //   responses: {
+  //     '204': {
+  //       description: 'Link PATCH success',
+  //     },
+  //   },
+  // })
+  // async updateById(
+  //   @param.path.number('id') id: number,
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(Link, {partial: true}),
+  //       },
+  //     },
+  //   })
+  //   link: Link,
+  // ): Promise<void> {
+  //   await this.linkRepository.updateById(id, link);
+  // }
 
-  @put('/links/{id}', {
-    responses: {
-      '204': {
-        description: 'Link PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.number('id') id: number,
-    @requestBody() link: Link,
-  ): Promise<void> {
-    await this.linkRepository.replaceById(id, link);
-  }
+  // @put('/links/{id}', {
+  //   responses: {
+  //     '204': {
+  //       description: 'Link PUT success',
+  //     },
+  //   },
+  // })
+  // async replaceById(
+  //   @param.path.number('id') id: number,
+  //   @requestBody() link: Link,
+  // ): Promise<void> {
+  //   await this.linkRepository.replaceById(id, link);
+  // }
 
-  @del('/links/{id}', {
-    responses: {
-      '204': {
-        description: 'Link DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.number('id') id: number): Promise<void> {
-    await this.linkRepository.deleteById(id);
-  }
+  // @del('/links/{id}', {
+  //   responses: {
+  //     '204': {
+  //       description: 'Link DELETE success',
+  //     },
+  //   },
+  // })
+  // async deleteById(@param.path.number('id') id: number): Promise<void> {
+  //   await this.linkRepository.deleteById(id);
+  // }
 }
--- src/services/AVConverter.ts
+++ src/services/AVConverter.ts
@@ -8,14 +8,24 @@ import youtubedl from 'youtube-dl';
  * @param video
  */
 async function convertToAudio(ref: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    const fsRef = await fs.createReadStream(`files/${ref}.mp4`);
+    return await ffmpeg()
+      .input(fsRef)
+      .output(`files/${ref}.mp3`)
+      .on('end', () => {
+        console.log("audio converted")
+        return resolve(`files/${ref}.mp3`)
+      }
+      )
+      .on('error', () => {
+        console.log('reject')
+        return reject();
+      }
+      )
+      .run();
+  })
 
-  //  TODO: implement fluent ffmpeg to convert a video to an audio file
-  const fsRef = fs.createReadStream(`files/${ref}`);
-  ffmpeg()
-    .input(fsRef)
-    .output(`files/${ref}.mp3`)
-    .run();
-  return `files/${ref}.mp3`
 }
 
 /**
@@ -23,36 +33,29 @@ async function convertToAudio(ref: string): Promise<string> {
  * @param url - url of the video to be downloaded
  */
 async function requestVideo(url: string) {
-  console.log(url);
-  let name = '';
-  const video = youtubedl(url,
-    // Optional arguments passed to youtube-dl.
-    ['--format=18'],
-    // Additional options can be given for calling `child_process.execFile()`.
-    {cwd: __dirname})
+  return new Promise((resolve, reject) => {
+    const video = youtubedl(`https://www.youtube.com/watch?v=${url}`,
+      ['--format=18'],
+      {cwd: __dirname});
 
-  // Will be called when the download starts.
-  video.on('info', function (info) {
-    console.log('Download started')
-    name = info._filename;
-    console.log(name);
-    video.pipe(fs.createWriteStream(`files/${info._filename}`, {flags: 'a'}));
-    console.log('filename: ' + info._filename)
-    console.log('size: ' + info.size)
-  })
-  // TODO: add error handling to avoid empty mp4 files
+    video.on('info', function (info) {
+      console.log('Download started')
+      console.log('filename: ' + info._filename)
+      console.log('size: ' + info.size)
 
-  video.on('complete', function complete(info) {
-    'use strict'
-    console.log('filename: ' + info._filename + ' already downloaded.')
+    })
 
-  })
 
-  video.on('end', function (info) {
-    console.log('finished downloading!')
-    convertToAudio(name)
-  })
+
+    video.on('end', async function () {
+      console.log('finished downloading!')
+      return resolve(convertToAudio(url));
+    })
+    video.on('error', (e) => e ? reject(e) : null)
+
+    video.pipe(fs.createWriteStream(`files/${url}.mp4`, {flags: 'a'}));
+  });
 }
 
-export {requestVideo};
+export {requestVideo, convertToAudio};
 
